f8da72f7b14047920729235fb79aebdedf5a1ad2
Sandro Antônio Souza <sandroantoniosouza98@gmail.com>
2022-06-16 19:23:55 -0300

    second commit

---
+++ db.js
@@ -0,0 +1,13 @@
+const artigos = require('./data/artigos');
+const comentarios = require('./data/comentarios');
+const favoritados = require('./data/favoritados');
+const jogos = require('./data/jogos');
+const usuarios = require('./data/usuarios');
+
+module.exports = {
+  artigos,
+  comentarios,
+  favoritados,
+  jogos,
+  usuarios
+}
